40e6bbe02d85b3695ac068d28987f67df04886c1
Arnaldo Castilla <arcastil@redhat.com>
2025-07-18 18:11:12 -0500

    implementando constantes globales del aplicativo

---
+++ src/main/java/com/challengeraven/calculator/app/config/Constants.java
@@ -0,0 +1,6 @@
+package com.challengeraven.calculator.app.config;
+
+public class Constants {
+	public static final String MSG_SQRT= "Raíz cuadrada solo de números positivos";
+	public static final String MSG_DIVIDE= "División por cero no permitida";
+}
